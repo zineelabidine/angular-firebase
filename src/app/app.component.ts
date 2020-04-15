827263f5ec7145e9368528aa8a9adc1614acceef
zineelabidin <bakherzineelabidine@gmail.com>
2020-04-15 14:55:56 +0100

    the application of angular with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,7 @@ export class AppComponent {
   title = 'angular-Firebase';
   constructor() {
     const config = {
-      apiKey: "AIzaSyCAlHruZTXVqkKFFASkaWXrJwkDo_7Mbuk",
-      authDomain: "angular-datebase.firebaseapp.com",
-      databaseURL: "https://angular-datebase.firebaseio.com",
-      projectId: "angular-datebase",
-      storageBucket: "angular-datebase.appspot.com",
-      messagingSenderId: "175017371011",
-      appId: "1:175017371011:web:b1bb4f6a8a49377096c2b6",
-      measurementId: "G-3BSQKW0GNG"
+   
     };
     firebase.initializeApp(config);
   }
